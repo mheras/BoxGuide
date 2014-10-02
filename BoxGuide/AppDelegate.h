72f29450ca1f7d0ee915cbf56f815212f05dcacf
Martin Heras <martin.heras@globant.com>
2014-10-01 22:02:27 -0300

    Initial import.

---
+++ BoxGuide/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BoxGuide
+//
+//  Created by Martin Heras on 10/1/14.
+//  Copyright (c) 2014 HSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BoxGuide/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BoxGuide
+//
+//  Created by Martin Heras on 10/1/14.
+//  Copyright (c) 2014 HSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
