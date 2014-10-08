feddfa63ce998da9983f4e627ba05b3b93603a26
Martin Heras <martin.heras@globant.com>
2014-10-08 09:36:10 -0300

    Added FontAwesome.

--- BoxGuide/BGViewController.m
+++ BoxGuide/BGViewController.m
@@ -7,11 +7,26 @@
 //
 
 #import "BGViewController.h"
+#import <RESideMenu.h>
+#import <NSString+FontAwesome.h>
+#import <UIFont+FontAwesome.h>
 
 @interface BGViewController ()
 
 @end
 
 @implementation BGViewController
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    if ([self.navigationController.viewControllers firstObject] == self) {
+        self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:[NSString fontAwesomeIconStringForIconIdentifier:@"icon-list"] style:UIBarButtonItemStylePlain target:self action:@selector(onRootMenuButtonTouch)];
+        [self.navigationItem.leftBarButtonItem setTitleTextAttributes:@{NSFontAttributeName: [UIFont fontAwesomeFontOfSize:17.0]} forState:UIControlStateNormal];
+    }
+}
+
+- (void)onRootMenuButtonTouch {
+    [self.sideMenuViewController presentLeftMenuViewController];
+}
 
 @end
--- Podfile
+++ Podfile
@@ -1,10 +1,11 @@
 source 'https://github.com/CocoaPods/Specs.git'
 platform :ios, '7.0'
 
+inhibit_all_warnings!
+
 pod 'RESideMenu', '~> 4.0'
 pod 'RestKit', '~> 0.23'
 pod 'BlocksKit', '~> 2.2'
 pod 'CrashlyticsFramework', '~> 2.2'
 pod 'CocoaLumberjack', '2.0.0-beta2'
-
-
+pod 'FontAwesome+iOS', '~> 0.1'
